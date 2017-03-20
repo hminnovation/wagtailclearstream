82a041db35ea055e8badcaf263504face4bf371d
Edd Baldry <edd@potatolondon.com>
2017-03-20 14:23:50 +0000

    Collapse streamfield member on load. Toggle open. Toggle delete button

--- wagtailbettereditor/static_src/wagtailbettereditor/js/clear_stream.js
+++ wagtailbettereditor/static_src/wagtailbettereditor/js/clear_stream.js
@@ -1,1 +1,23 @@
 // For various js fun
+$(document).ready(function() {
+    // find every .sequence-member in the dom
+    $('.sequence-member').each(function() {
+        var el = $(this);
+        var delDialog = $(this).find('.delete-dialog');
+
+        // add collapsed class
+        $(this).addClass('collapsed');
+
+        // find all buttons with class .collapse within this parent
+        $(this).find('button.collapse').on('click', function() {
+            // toggle the parent's class 'collapsed'
+            el.toggleClass('collapsed');
+        });
+
+        // find all buttons with class .open-toggle
+        $(this).find('button.open-toggle').on('click', function() {
+            delDialog.removeClass('closed');
+            delDialog.addClass('open');
+      });
+    });
+});
