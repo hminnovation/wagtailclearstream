f98a12a679496bbfbf52f68ad67653a81200e776
Patrick Gerken <patrick.gerken@zumtobelgroup.com>
2017-03-23 23:09:16 +0100

    Improving the drag and drop
    We now have (pretty ugly) drop areas for targeting the thingies.
    They only get shown where they make sense. The is now a drag handler.
    It only gets shown, when there is another position to drag to. We abuse
    the existing arrow up and down handlers. If one of those is visible we
    know we can change positions.
    
    I think it would be nice to have some animation for the dropboxes to
    appear. Also, we can change the cursor during drag. And the dropboxes
    are ugly.

--- wagtailclearstream/static/wagtailclearstream/js/clear_stream.js
+++ wagtailclearstream/static/wagtailclearstream/js/clear_stream.js
@@ -4,7 +4,40 @@ $(document).ready(function() {
 
     function MakeDraggable(){
         var el = $(this),
-            parent_id = el.parent().attr('id');
+            parent_id = el.parent().attr('id'),
+            move_buttons = el.find(".sequence-controls:first").find(" button.icon-order-down, button.icon-order-up").filter(":visible");
+        if(move_buttons.length===0){
+            // neither up nor down button is visible, so movement possible
+            return;
+        }
+        move_buttons.last().after($("<button type=\"button\" title=\"Drag up or down\" class=\"drag-handle button icon text-replace icon-grip\"></button>"));
+        move_buttons.hide();
+
+        function makeDropTarget(){
+            return $("<li class=\"drop-target\">Drop it here!</li>");
+        }
+
+        function StartAction(){
+            var el = $(this),
+                widget = el.data('ui-draggable'),
+                id = el.attr("id"),
+                prev = el.prev(),
+                neighbours;
+            el.parent().children(":first:not(#" + id + ")").before(makeDropTarget());
+            neighbours = el.parent().children(":not(#" + id + "):not(.drop-target)");
+            if(prev.length == 1){
+                neighbours = neighbours.filter(":not(#" + prev.attr("id") + ")");
+            }
+            neighbours.after(makeDropTarget());
+            // We changed the html and must recalculate the valid containment area
+            // and the valid drag targets
+            widget._setContainment();
+            for (var i = 0; i < widget.plugins.start.length; i++) {
+                if (widget.plugins.start[i][0] === "snap"){
+                    widget.plugins.start[i][1].call(this);
+                }
+            }
+        }
 
         function StopAction(){
             var valid_snap_targets = $(this).data('ui-draggable').snapElements,
@@ -16,14 +49,21 @@ $(document).ready(function() {
             if(snapped_to.length === 1){
                 snapped_to = $(snapped_to[0]);
             }else{
+                $(".drop-target").remove();
                 return;
             }
 
             function extract_position(elem){
-                return elem.parent().children().index(elem);
+                return elem.parent().children(":not(.drop-target)").index(elem);
             }
             my_position = extract_position(el);
-            target_position = extract_position(snapped_to);
+            if(snapped_to.prev().length === 1){
+                target_position = extract_position(snapped_to.prev());
+            } else {
+                // There is no previous element, meaning that we are in the very first
+                // drop box.
+                target_position = 0;
+            }
             if (my_position > target_position){
                 click_target = $(this).find('.icon-order-up');
             }else{
@@ -32,15 +72,31 @@ $(document).ready(function() {
             for(var i=0;i<Math.abs(my_position - target_position);i++){
                 click_target.click();
             }
+            $(".drop-target").remove();
         }
+
+        function CloneFunction(){
+            var clone = $(this).clone();
+            clone.addClass("collapsed")
+                .find('button.collapse')
+                .addClass("icon-plus")
+                .removeClass("icon-collapse-up");
+            return clone;
+        }
+
         el.draggable({
             scroll: true,
             axis: "y",
             containment: "parent",
             revert: true,
             revertDuration: 0,
-            snap: "#" + parent_id + " > .sequence-member",
-            snapMode: "outer",
+            snap: ".drop-target",
+            handle: ".drag-handle",
+            cancel: "", // Do not cancle on button.drag-handle
+            snapMode: "inner",
+            refreshPositions: true,
+            helper: CloneFunction,
+            start: StartAction,
             stop: StopAction,
         });
     }
@@ -76,14 +132,14 @@ $(document).ready(function() {
     // of the parent stream-field element and allow the div to be moved
     // at the speed with which the user is scrolling
     // Problem with this is it counts from both top and bottom
-    $(window).scroll(function(){
-        var scroll_top = $(this).scrollTop(); // get scroll position top
-        var height_element_parent =  $(".block_field.stream-field h2").parent().outerHeight(); //get high parent element
-        var height_element = $(".block_field.stream-field h2").height(); //get high of elemeneto
-        var position_fixed_max = height_element_parent - height_element; // get the maximum position of the elemen
-        var position_fixed = scroll_top < 250 ? 250 - scroll_top : position_fixed_max > scroll_top ? 0 : position_fixed_max - scroll_top;
-        $(".block_field.stream-field h2").css("top",position_fixed);
-    });
+    // $(window).scroll(function(){
+    //     var scroll_top = $(this).scrollTop(); // get scroll position top
+    //     var height_element_parent =  $(".block_field.stream-field h2").parent().outerHeight(); //get high parent element
+    //     var height_element = $(".block_field.stream-field h2").height(); //get high of elemeneto
+    //     var position_fixed_max = height_element_parent - height_element; // get the maximum position of the elemen
+    //     var position_fixed = scroll_top < 250 ? 250 - scroll_top : position_fixed_max > scroll_top ? 0 : position_fixed_max - scroll_top;
+    //     $(".block_field.stream-field h2").css("top",position_fixed);
+    // });
 
     // Newly added elements need buttons set up become draggable
     document.addEventListener("DOMNodeInserted", function(event) {
