1948c0e37e9328a21122e00f79bdaa45b8f48cd0
adrian-turjak <adriant@catalyst.net.nz>
2017-06-09 14:02:25 +1200

    Better handling of admin CSS and JS
    * Ensure static files are properly linked.
    * By using this method even external object storage
      based static file storage will just work.

--- wagtailclearstream/wagtail_hooks.py
+++ wagtailclearstream/wagtail_hooks.py
@@ -1,21 +1,27 @@
 from pkg_resources import parse_version
 
 from django.utils.html import format_html
-from django.conf import settings
+from django.contrib.staticfiles.templatetags.staticfiles import static
 
 from wagtail.wagtailcore import hooks
 from wagtail.wagtailcore import __version__ as WAGTAIL_VERSION
 
+
 @hooks.register('insert_global_admin_css')
 def global_admin_css():
-    elem = '<link rel="stylesheet" href="%swagtailclearstream/css/wagtailclearstream.css">' % settings.STATIC_URL
-    return format_html(elem)
+    return format_html(
+        '<link rel="stylesheet" href="{}">',
+        static('wagtailclearstream/css/wagtailclearstream.css')
+    )
+
 
 @hooks.register('insert_global_admin_js')
 def global_admin_js():
     return format_html(
-        '<script src="/static/wagtailclearstream/js/clear_stream.js"></script>',
+        '<script src="{}"></script>',
+        static('wagtailclearstream/js/clear_stream.js')
     )
+
 
 # New Wagtail versions support importing CSS throughout the admin.
 # Fall back to the old hook (editor screen only) for older versions.
